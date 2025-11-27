b1141f25546254da54922cfb8e8f31e29d974f26
AldoRodriguez28 <arodriguez.ronquillo@hotmail.com>
2025-11-27 12:45:24 -0600

    firstcommit for dynamics form application

---
+++ src/app/components/field-multiselect/field-multiselect.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormField, OptionItem } from '../../models/form-schema.model';
+
+@Component({
+  selector: 'app-field-multiselect',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './field-multiselect.component.html',
+  styleUrl: './field-multiselect.component.scss'
+})
+export class FieldMultiselectComponent {
+  @Input({ required: true }) field!: FormField & { name: string };
+  @Input({ required: true }) control!: FormControl;
+  @Input() options: OptionItem[] = [];
+
+  get size(): number {
+    const base = this.options?.length ?? 4;
+    return Math.min(8, Math.max(4, base || 4));
+  }
+}
