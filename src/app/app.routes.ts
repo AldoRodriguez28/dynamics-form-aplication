1474412ccc5b102a3509845b177de45a2f68241b
AldoRodriguez28 <arodriguez.ronquillo@hotmail.com>
2026-01-14 23:30:51 -0600

    vista de usuairo no autorizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { BusinessListComponent } from './business-list/business-list.component';
 import { BusinessFormComponent } from './business-form/business-form.component';
 import { authByTokenResolver } from './resolvers/auth-by-token.resolver';
+import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
 
 export const routes: Routes = [
+  { path: 'unauthorized', component: UnauthorizedComponent },
   { path: ':idClient/:businessId', component: BusinessFormComponent, resolve: { authReady: authByTokenResolver } },
   { path: ':idClient', component: BusinessListComponent, resolve: { authReady: authByTokenResolver } },
   { path: '', pathMatch: 'full', redirectTo: '2009786511' },
